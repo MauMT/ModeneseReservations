900cc10d69e782caf9a322e3059733b149d068e5
JazminSantibanez <A01234029@itesm.mx>
2022-05-11 15:01:35 -0500

    Fetch data from db and show in products

--- front/src/views/productos.js
+++ front/src/views/productos.js
@@ -8,7 +8,7 @@ import {
   CardContent,
 } from '@material-ui/core';
 import React from 'react';
-import productos from '../config/productos.json';
+//import productos from '../config/productos.json';
 import axios from 'axios';
 
 const styles = makeStyles((theme) => ({
@@ -48,23 +48,23 @@ const Productos = () => {
   return (
     <Container className={style.cardGrid} maxWidth="md">
       <Grid container spacing={6}>
-        {productos.map((producto) => (
-          <Grid item key={producto.title} xs={12} sm={6} md={4}>
+        {products.map((producto) => (
+          <Grid item key={producto.nombreProducto} xs={12} sm={6} md={4}>
             <Card className={style.card}>
               <CardMedia
                 className={style.cardMedia}
-                image={producto.image}
+                image={producto.imagen}
                 title="Image title"
               />
               <CardContent className={style.cardContent}>
                 <Typography gutterBottom variant="h5" component="h2">
-                  {producto.title}
+                  {producto.nombreProducto}
                 </Typography>
                 <Typography gutterBottom variant="body1">
                   <strong>Precio: </strong>
                   {producto.precio}
                 </Typography>
-                <Typography variant="body2">{producto.description}</Typography>
+                {/* <Typography variant="body2">{producto.description}</Typography> */}
               </CardContent>
             </Card>
           </Grid>

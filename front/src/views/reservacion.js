aaef37c206dd84ce3eed89ca9005acb20558afa7
Mauricio Martinez Toledo <mauriciomtzt@hotmail.com>
2022-06-11 19:32:05 -0500

    first commit

--- front/src/views/reservacion.js
+++ front/src/views/reservacion.js
@@ -243,7 +243,7 @@ const Contact = () => {
                           <MenuItem value={2}> 2 </MenuItem>
                           <MenuItem value={3}> 3 </MenuItem>
                           <MenuItem value={4}> 4 </MenuItem> 
-                        
+                          <MenuItem value={5}> 5 </MenuItem>
                       </Select>
                     </FormControl>
                   </Box>
